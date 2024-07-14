68f2a4724666aa5a06e8cdb9ecb98f368564d191
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-07-15 01:30:58 +0300

    use type parameters

--- spinner.go
+++ spinner.go
@@ -2,17 +2,19 @@
 // go beyond the end value.
 package spinner
 
+import "golang.org/x/exp/constraints"
+
 // Iterates over integer values, while returning the begin value when trying to
 // go beyond the end value.
-type Spinner struct {
-	actual int
-	begin  int
-	end    int
+type Spinner[Type constraints.Integer] struct {
+	actual Type
+	begin  Type
+	end    Type
 }
 
 // Creates Spinner instance.
-func New(begin int, end int) *Spinner {
-	spn := &Spinner{
+func New[Type constraints.Integer](begin Type, end Type) *Spinner[Type] {
+	spn := &Spinner[Type]{
 		actual: begin,
 		begin:  begin,
 		end:    end,
@@ -22,17 +24,17 @@ func New(begin int, end int) *Spinner {
 }
 
 // Returns actual value of counter.
-func (spn *Spinner) Actual() int {
+func (spn *Spinner[Type]) Actual() Type {
 	return spn.actual
 }
 
 // Increases the current value of the counter, if its next value exceeds the end value,
 // it will be reset to the begin value.
-func (spn *Spinner) Spin() {
+func (spn *Spinner[Type]) Spin() {
 	spn.actual = spn.spin()
 }
 
-func (spn *Spinner) spin() int {
+func (spn *Spinner[Type]) spin() Type {
 	if spn.actual >= spn.end {
 		return spn.begin
 	}
