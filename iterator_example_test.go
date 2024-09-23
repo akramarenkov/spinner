5bb98ba317ce44328a0a4f2c69d525432cc23d9c
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-09-23 09:58:42 +0300

    added iter examples

---
+++ iterator_example_test.go
@@ -0,0 +1,55 @@
+package spinner_test
+
+import (
+	"fmt"
+
+	"github.com/akramarenkov/spinner"
+)
+
+func ExampleIter() {
+	iterations := 0
+
+	for number := range spinner.Iter(-1, 2) {
+		fmt.Println(number)
+
+		iterations++
+
+		if iterations == 8 {
+			return
+		}
+	}
+
+	// Output:
+	// -1
+	// 0
+	// 1
+	// 2
+	// -1
+	// 0
+	// 1
+	// 2
+}
+
+func ExampleIterStep() {
+	iterations := 0
+
+	for number := range spinner.IterStep(-1, 2, 2) {
+		fmt.Println(number)
+
+		iterations++
+
+		if iterations == 8 {
+			return
+		}
+	}
+
+	// Output:
+	// -1
+	// 1
+	// 2
+	// -1
+	// 1
+	// 2
+	// -1
+	// 1
+}
